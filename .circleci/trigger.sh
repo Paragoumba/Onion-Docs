0f866c219bd5c1bd0a2302332f42f96d7dd8e77b
greenbreakfast <lazardemin@gmail.com>
2018-12-15 19:34:55 -0500

    added script to trigger website rebuild

---
+++ .circleci/trigger.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -X POST --header "Content-Type: application/json" https://circleci.com/api/v1.1/project/$(TARGET_VCS)/$(TARGET_USERNAME)/:$(TARGET_PROJECT)/build?circle-token=$(CIRCLE_API_KEY)
